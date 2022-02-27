677279c84bfc783e037f46040e97defd4ae9907b
Natalia Chrząścik <56516256+nataliachrzascik@users.noreply.github.com>
2022-02-27 18:26:12 +0100

    Update app.js

--- api/backend/app.js
+++ api/backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+var path = require('path');
 // const dbConfig = require('./config/dbConfig.js');
 
 
