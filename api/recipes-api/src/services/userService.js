f9b6fc7ff622c396dfa03bf2cf67d80d0fca97c2
Natalia Chrząścik <56516256+nataliachrzascik@users.noreply.github.com>
2022-02-27 18:54:40 +0100

    Update userService.js

--- api/recipes-api/src/services/userService.js
+++ api/recipes-api/src/services/userService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './authHeader';
 
-const API_URL = 'http://localhost:8080/api/test/';
+const API_URL = 'https://dyplomowe.herokuapp.com/api/test/';
 
 class UserService {
   getPublicContent() {
