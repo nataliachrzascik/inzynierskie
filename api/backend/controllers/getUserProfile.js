2e50220c8bcc70cad04c446e10753bb444767b74
Natalia Chrząścik <56516256+nataliachrzascik@users.noreply.github.com>
2022-02-28 11:10:36 +0100

    Update getUserProfile.js

--- api/backend/controllers/getUserProfile.js
+++ api/backend/controllers/getUserProfile.js
@@ -1,10 +1,6 @@
-const config = require("../config/authConfig.js");
 const db = require("../models");
 const Recipes = db.recipes;
 
-
-var jwt = require("jsonwebtoken");
-var bcrypt = require("bcryptjs");
 
 exports.getUserProfile = (req, res) => {
   let nickUrl=req.url.substring(11);
